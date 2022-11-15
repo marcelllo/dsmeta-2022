d85c97d1908d9707a26b1b3bad1b11f295a07c16
Marcelo Santos <marcello.santos@gmail.com>
2022-11-15 19:59:47 -0300

    Creating Notification Button

--- dsmeta-frontend/src/App.tsx
+++ dsmeta-frontend/src/App.tsx
@@ -1,5 +1,13 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
-  return <h1>DSMeta</h1>;
+  return (
+    <>
+      <h1>DSMeta</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App;
