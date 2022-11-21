4f1dcb936c20e26b49cc5bbefb85c300daf3fd2b
Marcelo Santos <marcello.santos@gmail.com>
2022-11-21 01:25:14 -0300

    Toast

--- dsmeta-frontend/src/components/NotificationButton/index.tsx
+++ dsmeta-frontend/src/components/NotificationButton/index.tsx
@@ -3,6 +3,7 @@ import icon from "../../assets/img/notification-icon.svg";
 import "./styles.css";
 import axios from "axios";
 import {BASE_URL} from "../../utils/request";
+import {toast} from "react-toastify";
 
 interface NotificationButtonProps {
     saleId: number;
@@ -12,7 +13,7 @@ function NotificationButton({ saleId }: NotificationButtonProps) {
 
     const handleClick = () => {
         axios.get(`${BASE_URL}/sales/${saleId}/notification`)
-            .then(response => console.log('SUCESSO'));
+            .then(response => toast.info('SMS enviado com sucesso'));
     }
 
   return (
